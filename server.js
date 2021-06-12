086a114f6ec37c92e64e9993872ccba5a35acdbc
Chelsea L <chelsealumsden@gmail.com>
2021-06-12 18:49:15 -0400

    Mongoose connected

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+// Dependencies
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+// Database Config
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
 
-
+// Database Connection Check
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 
 
